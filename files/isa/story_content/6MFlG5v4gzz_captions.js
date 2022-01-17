e26e3b9858c21c4c6e0394b3a9e6488a35e77f33
Name <midhatk@sfu.ca>
2022-01-17 15:45:28 -0800

    Removed ISA prototype files (for now)

--- files/isa/story_content/6MFlG5v4gzz_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.847%0D%0AQuestions%20may%20be%20directed%20to%20Faculty%20Relations.%0D%0A%0D%0A"}')
