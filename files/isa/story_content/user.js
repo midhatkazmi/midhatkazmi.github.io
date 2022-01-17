f722c63e5d1228958b71337041d7f231b72895c5
Name <midhatk@sfu.ca>
2022-01-17 15:30:08 -0800

    Uploaded ISA project page & associated files

---
+++ files/isa/story_content/user.js
@@ -0,0 +1,183 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6aOjFukGM2y":
+        Script1();
+        break;
+      case "6h2vwz6hkzt":
+        Script2();
+        break;
+      case "5rCBQFvrVLN":
+        Script3();
+        break;
+      case "5qRFLx0Vzm2":
+        Script4();
+        break;
+      case "6eSAKeg7p0f":
+        Script5();
+        break;
+      case "649wMKhdf5E":
+        Script6();
+        break;
+      case "65l26YBhbe4":
+        Script7();
+        break;
+      case "5zhRXXDzmyu":
+        Script8();
+        break;
+      case "6q0FDJfN7Ii":
+        Script9();
+        break;
+      case "6bwP1E0sxBZ":
+        Script10();
+        break;
+      case "6EF115hXNqZ":
+        Script11();
+        break;
+      case "5pnDnScG87N":
+        Script12();
+        break;
+      case "66PlnTOTKYo":
+        Script13();
+        break;
+      case "62dZSSqEz5l":
+        Script14();
+        break;
+      case "5ovnYQvUyqw":
+        Script15();
+        break;
+      case "5mwrSHHFuxJ":
+        Script16();
+        break;
+      case "5dnEw0R2PB9":
+        Script17();
+        break;
+      case "5lZLel10ggr":
+        Script18();
+        break;
+      case "67eahiNAbN7":
+        Script19();
+        break;
+      case "6TyaDzM7xfn":
+        Script20();
+        break;
+      case "6dtQZXg5cbw":
+        Script21();
+        break;
+      case "5qxH7gFC6ri":
+        Script22();
+        break;
+  }
+}
+
+function Script1()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script2()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script3()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script4()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script5()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script6()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script7()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script8()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script9()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script10()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script11()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script12()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script13()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script14()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script15()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script16()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script17()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script18()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script19()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script20()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script21()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script22()
+{
+  DS.appState.onToggleVolume();
+}
+
