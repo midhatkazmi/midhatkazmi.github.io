e2e4ece69558cdc4acdc130622b75dc77c46ea53
Name <midhatk@sfu.ca>
2022-01-18 12:34:17 -0800

    Reuploaded ISA prototype files

---
+++ files/isa/story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "66YiY15fdx0":
+        Script1();
+        break;
+      case "6dxdhYBGaW4":
+        Script2();
+        break;
+      case "6ZxlcMu4pHl":
+        Script3();
+        break;
+      case "6ZZ2rf8XTqU":
+        Script4();
+        break;
+      case "6Pb8TQ8ybm8":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script2()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script3()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script4()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script5()
+{
+  DS.appState.onToggleVolume();
+}
+
