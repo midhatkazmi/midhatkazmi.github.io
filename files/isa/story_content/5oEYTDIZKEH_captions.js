f2eaa31b44221f7e071f98c1efe6902af4fef505
Name <midhatk@sfu.ca>
2022-03-03 02:24:16 -0800

    Updated isa.html demo

---
+++ files/isa/story_content/5oEYTDIZKEH_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.180%0D%0AInstructions%20detailing%20what%20to%20submit,%20who%20to%20submit%20to%20and%20how%20to%20submit%0D%0A%0D%0A"}')
