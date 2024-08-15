4a998b47bbce9b1c229e71e42d97ab043dbc0b74
midhatkazmi <42825909+midhatkazmi@users.noreply.github.com>
2024-08-14 17:38:39 -0700

    Delete files directory

--- files/isa/story_content/63xhHD5JYIr_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.030%0D%0ARelevant%20university%20\u0026%20departmental%20information%0D%0A%0D%0A"}')
