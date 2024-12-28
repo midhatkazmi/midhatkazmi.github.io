89e7b90d12004cd7debeff7a425a22917c993976
Midhat <midhatk@sfu.ca>
2024-12-27 22:26:29 -0800

    test

---
+++ files/isa/story_content/5yaeDXnQmVK_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.056%0D%0ADescription%20of%20responsibilities%20and%20expectations%0D%0A%0D%0A"}')
---
+++ files/isa/story_content/user.js
@@ -0,0 +1,239 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "65oBGdlOx7O":
+        Script1();
+        break;
+      case "5sQf0yV1OtJ":
+        Script2();
+        break;
+      case "6Szp1mgWyz6":
+        Script3();
+        break;
+      case "6iOacstX9t0":
+        Script4();
+        break;
+      case "6Uo0JkREiex":
+        Script5();
+        break;
+      case "6pYKg466qKI":
+        Script6();
+        break;
+      case "6gyQGMwYoeG":
+        Script7();
+        break;
+      case "6IJf1uCrd4k":
+        Script8();
+        break;
+      case "5pA4JrG4lSk":
+        Script9();
+        break;
+      case "5zCY3RCDR90":
+        Script10();
+        break;
+      case "5vPf4VFkHnr":
+        Script11();
+        break;
+      case "5xEPg1fY9l1":
+        Script12();
+        break;
+      case "65sLvHb5RLM":
+        Script13();
+        break;
+      case "6ocXqL6EsVh":
+        Script14();
+        break;
+      case "6HT7A8bBqnu":
+        Script15();
+        break;
+      case "6T4l0Omso8b":
+        Script16();
+        break;
+      case "66YhSybRmHD":
+        Script17();
+        break;
+      case "5VOeBGx2ROU":
+        Script18();
+        break;
+      case "6m4n8aaBJGn":
+        Script19();
+        break;
+      case "5ryfIfqZrSY":
+        Script20();
+        break;
+      case "5vZB4OdeMBn":
+        Script21();
+        break;
+      case "6PyWB1hqtW0":
+        Script22();
+        break;
+      case "62w2a25sEPx":
+        Script23();
+        break;
+      case "5wWMLxro1AH":
+        Script24();
+        break;
+      case "6ZUPB75mh0b":
+        Script25();
+        break;
+      case "6MMIJaLCxzR":
+        Script26();
+        break;
+      case "5zLkpoEIXbV":
+        Script27();
+        break;
+      case "62ItJyKuHmx":
+        Script28();
+        break;
+      case "61cYh62IiUF":
+        Script29();
+        break;
+  }
+}
+
+function Script1()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script2()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script3()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script4()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script5()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script6()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script7()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script8()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script9()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script10()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script11()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script12()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script13()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script14()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script15()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script16()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script17()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script18()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script19()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script20()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script21()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script22()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script23()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script24()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script25()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script26()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script27()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script28()
+{
+  DS.appState.onToggleVolume();
+}
+
+function Script29()
+{
+  DS.appState.onToggleVolume();
+}
+
