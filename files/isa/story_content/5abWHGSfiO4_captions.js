d7799d60f6325a2152514c2cb4525b3686c4bb92
Midhat <midhatk@sfu.ca>
2024-12-31 14:23:54 -0800

    Rebrand

--- files/isa/story_content/5abWHGSfiO4_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.415%0D%0AExpertise%20on%20pedagogic%20effectiveness%20and%20use%20of%20teaching%20portfolios.%0D%0A%0D%0A"}')
--- files/isa/story_content/5yn1t6opNdB_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.56.26219.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.389%0D%0AClear%20statement%20of%20essential%20qualifications%20and%20evaluation%20criteria%0D%0A%0D%0A"}')
